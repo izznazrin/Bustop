fbf7d034ea40f34a512f8ff0174fbef74558ef72
izznazrin <nazrin.izryzza@gmail.com>
2023-06-06 00:20:10 +0800

    6/6/2023

--- lib/journey.dart
+++ lib/journey.dart
@@ -1,7 +1,154 @@
 import 'package:flutter/material.dart';
 import 'package:bustop/busstoplist.dart';
+import 'dart:math';
+import 'package:geolocator/geolocator.dart';
+import 'package:url_launcher/url_launcher.dart';
 
-class JourneyModule extends StatelessWidget {
+class BusStop {
+  final String name;
+  final double latitude;
+  final double longitude;
+
+  BusStop(
+      {required this.name, required this.latitude, required this.longitude});
+}
+
+// Sample list of bus stops
+List<BusStop> busStops = [
+  BusStop(
+      name: 'Kolej Kediaman Pewira', latitude: 1.861775, longitude: 103.099222),
+  BusStop(
+      name: 'PKU & CARE',
+      latitude: 1.8561335089285704,
+      longitude: 103.08425674117098),
+  BusStop(
+      name: 'G3 (Back)',
+      latitude: 1.8578237774971171,
+      longitude: 103.08711181778779),
+  BusStop(name: 'FKEE', latitude: 1.859955, longitude: 103.089037),
+  BusStop(
+      name: 'Kolej Kediaman Tun Fatimah',
+      latitude: 1.863189,
+      longitude: 103.088996),
+  BusStop(
+      name: 'Kolej Kediaman Tun Dr. Ismail',
+      latitude: 1.862917,
+      longitude: 103.089213),
+  BusStop(name: 'G3 (Front)', latitude: 1.859658, longitude: 103.086544),
+  BusStop(name: 'Library', latitude: 1.858096, longitude: 103.083229),
+  BusStop(
+      name: 'Dewan Sultan Ibrahim', latitude: 1.858260, longitude: 103.081370),
+  BusStop(name: 'FPTP', latitude: 1.861238, longitude: 103.081408),
+  BusStop(name: 'FPTV', latitude: 1.862954, longitude: 103.081570),
+  BusStop(name: 'FKAAB', latitude: 1.864361, longitude: 103.083192),
+  BusStop(name: 'FKAAB 2', latitude: 1.864114, longitude: 103.085262),
+  BusStop(
+      name: 'FKEE (New Building)', latitude: 1.860598, longitude: 103.086141),
+  BusStop(name: 'FKMP', latitude: 1.861335, longitude: 103.087968),
+  BusStop(name: 'FKEE', latitude: 1.860276, longitude: 103.089091),
+  BusStop(name: 'Arked', latitude: 1.857055, longitude: 103.087494),
+  BusStop(name: 'ATM', latitude: 1.854348, longitude: 103.086318),
+];
+
+class JourneyModule extends StatefulWidget {
+  @override
+  JourneyModuleState createState() => JourneyModuleState();
+}
+
+class JourneyModuleState extends State<JourneyModule> {
+  String nearestBusStop = '';
+  int selectedBusStopIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    determineNearestBusStop(); // Call the function to determine the nearest bus stop on widget initialization
+  }
+
+  //code untuk dapatkan user current location
+  Future<Position> getCurrentLocation() async {
+    LocationPermission permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied ||
+        permission == LocationPermission.deniedForever) {
+      permission = await Geolocator.requestPermission();
+      if (permission != LocationPermission.whileInUse &&
+          permission != LocationPermission.always) {
+        // Handle location permission denied
+        return Future.error('Location permission denied');
+      }
+    }
+
+    Position position = await Geolocator.getCurrentPosition(
+      desiredAccuracy: LocationAccuracy.high,
+    );
+
+    return position;
+  }
+
+  void determineNearestBusStop() async {
+    /// Retrieve the user's current location coordinates
+    Position position = await getCurrentLocation();
+    final userLatitude = position.latitude;
+    final userLongitude = position.longitude;
+
+    double minDistance = double.infinity;
+    String nearestStopName = '';
+
+    // Calculate the distance between the user's location and each bus stop
+    for (var busStop in busStops) {
+      final distance = calculateDistance(
+          userLatitude, userLongitude, busStop.latitude, busStop.longitude);
+      if (distance < minDistance) {
+        minDistance = distance;
+        nearestStopName = busStop.name;
+      }
+    }
+
+    setState(() {
+      nearestBusStop = nearestStopName;
+      selectedBusStopIndex =
+          busStops.indexWhere((busStop) => busStop.name == nearestStopName);
+    });
+  }
+
+  double calculateDistance(double lat1, double lon1, double lat2, double lon2) {
+    // Implementation of the Haversine formula to calculate distance between two coordinates
+    // You can use any other distance calculation formula of your choice
+
+    // Convert latitude and longitude to radians
+    final dLat = _toRadians(lat2 - lat1);
+    final dLon = _toRadians(lon2 - lon1);
+
+    // Earth's radius in kilometers
+    const earthRadius = 6371.0;
+
+    // Haversine formula
+    final a = sin(dLat / 2) * sin(dLat / 2) +
+        cos(_toRadians(lat1)) *
+            cos(_toRadians(lat2)) *
+            sin(dLon / 2) *
+            sin(dLon / 2);
+    final c = 2 * atan2(sqrt(a), sqrt(1 - a));
+    final distance = earthRadius * c;
+
+    return distance;
+  }
+
+  double _toRadians(double degrees) {
+    return degrees * pi / 180;
+  }
+
+  void launchGoogleMapsNavigation(double latitude, double longitude) async {
+    final address = '$latitude, $longitude';
+    final url = 'google.navigation:q=$address&mode=w';
+
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch Google Maps navigation';
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -96,7 +243,7 @@ class JourneyModule extends StatelessWidget {
                             children: [
                               Flexible(
                                 child: Text(
-                                  'FKEE (New Building)',
+                                  nearestBusStop,
                                   textAlign: TextAlign.center,
                                   style: TextStyle(
                                     fontSize: 20,
@@ -118,7 +265,14 @@ class JourneyModule extends StatelessWidget {
                                 ),
                                 child: ElevatedButton(
                                   onPressed: () {
-                                    // Add your button functionality here
+                                    if (selectedBusStopIndex >= 0 &&
+                                        selectedBusStopIndex <
+                                            busStops.length) {
+                                      final busStop =
+                                          busStops[selectedBusStopIndex];
+                                      launchGoogleMapsNavigation(
+                                          busStop.latitude, busStop.longitude);
+                                    }
                                   },
                                   child: Text(
                                     'Navigate',
