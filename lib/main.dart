a6566fa4e4300f6682f1b2e1fe12719b20563f0c
izznazrin <nazrin.izryzza@gmail.com>
2023-06-11 16:36:02 +0800

    11/6/2023

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(//dibah
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Auth(),
     );
